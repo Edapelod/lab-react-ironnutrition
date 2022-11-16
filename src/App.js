b3d85d5f0c84534018bb565a8d13ba686c1672d2
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-16 17:40:20 +0100

    iteration4done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import foodData from './foods.json';
 import { useState } from 'react';
 import React from 'react';
 import FoodBox from './components/foodBox';
+import FoodForm from './components/AddFoodForm';
 
 function App() {
-  const [foods, setFood] = useState(foodData);
+  const [foods, setFoods] = useState(foodData);
   return (
     <div key={foods.id}>
       {foods.map((food) => (
         <FoodBox
-          title={food.title}
+          key={food.servings + food.name}
+          title={food.name}
           calories={food.calories}
           image={food.image}
           servings={food.servings}
         />
       ))}
+      <FoodForm foods={foods} setFoods={setFoods} />
     </div>
   );
 }
